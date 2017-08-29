d813e5989281454ed25d7ba7fbe823b195194ee2
Paul Guevarra <p.a.guevarra@gmail.com>
2017-08-29 04:54:01 -0700

    update Song file with a constructor setter and getter methods

--- src/main/java/models/Song.java
+++ src/main/java/models/Song.java
@@ -1,4 +1,68 @@
 package models;
 
 public class Song {
+    private String songName;
+    private String genre;
+    private String subgenre;
+    private int id;
+
+    public Song(String songName, String genre, String subgenre){
+        this.songName=songName;
+        this.genre=genre;
+        this.subgenre=subgenre;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getSubgenre() {
+        return subgenre;
+    }
+
+    public void setSubgenre(String subgenre) {
+        this.subgenre = subgenre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Song song = (Song) o;
+
+        if (id != song.id) return false;
+        if (!songName.equals(song.songName)) return false;
+        if (!genre.equals(song.genre)) return false;
+        return subgenre.equals(song.subgenre);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = songName.hashCode();
+        result = 31 * result + genre.hashCode();
+        result = 31 * result + subgenre.hashCode();
+        result = 31 * result + id;
+        return result;
+    }
 }
